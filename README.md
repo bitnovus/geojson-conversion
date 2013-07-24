4f52b994b819d4be18e96a4962d9d27a70cd9955
Daniel Chyan <dchyan1@gmail.com>
2013-07-24 14:49:12 -0400

    Improving usability/readability

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 GeoJSON-Conversion
 ==================
+
+Please note there is a dependency on `json-simple`. It is available at http://mvnrepository.com/artifact/com.googlecode.json-simple/json-simple/1.1.1/.
--- src/com/convert/GeoConvert.java
+++ src/com/convert/GeoConvert.java
@@ -12,6 +12,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 public class GeoConvert {
+	public static final String INTERNAL_DELIMITER = " | ";
+	public static final String INTERNAL_DESIGNATOR = "=";
 
 	private static String readFile(FileReader fileIn) {
 		BufferedReader in = new BufferedReader(fileIn);
@@ -29,15 +31,15 @@ public class GeoConvert {
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 *  { "type": "Feature",
-     *    "geometry": {
-     *        "type": "LineString",
-     *        "coordinates": [
-     *           [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
-     *           ]
-     *       }
+	 *    "geometry": {
+	 *        "type": "LineString",
+	 *        "coordinates": [
+	 *           [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+	 *           ]
+	 *       }
 	 * 
 	 * @param args
 	 */
@@ -45,9 +47,9 @@ public class GeoConvert {
 		// TODO Auto-generated method stub
 		FileReader fileIn;
 		String jsonText = "";
-		
+
 		try {
-			fileIn = new FileReader("N:\\sample_json_data.txt");
+			fileIn = new FileReader("examples/tmobile_demo_data/20130608125914_208.54.90.246_51b31c92e3c2f.json");
 			jsonText = readFile(fileIn);
 
 		} catch (FileNotFoundException e) {
@@ -55,65 +57,98 @@ public class GeoConvert {
 			e.printStackTrace();
 		}
 
-		  JSONParser parser = new JSONParser();
-		                
-		  try{
-		    JSONObject jsobj = (JSONObject) parser.parse(jsonText);
-		    GeoJSONFeature feature = new GeoJSONFeature();
-		    JSONArray tempCoordinates = new JSONArray();
-		    
-		    JSONArray metrics = (JSONArray) jsobj.get("metrics");
-		    JSONArray tests = (JSONArray) jsobj.get("tests");
-		    JSONArray conditions = (JSONArray) jsobj.get("conditions");
-		    Set coll = jsobj.entrySet();
-		    for (Object temp : coll) {
-		    	System.out.println(temp.toString());
-		    }
- 
-		    for (Object temp : metrics) {
-		    	JSONObject tempjsobj = (JSONObject) temp;
+		JSONParser parser = new JSONParser();
 
-		    	if (tempjsobj.containsKey("longitude") && tempjsobj.containsKey("latitude")) {
-			    	JSONArray tempArray = new JSONArray();
-			    	tempArray.add(tempjsobj.get("latitude"));
-			    	tempArray.add(tempjsobj.get("longitude"));
-		    		
-			    	tempCoordinates.add(tempArray);
-			    	
-		    		//System.out.println(tempjsobj.toString());
-		    	}
-		    }
-		    GeoJSONGeometry geometry = new GeoJSONGeometry("LineString", tempCoordinates);
-		    feature.addGeometry(geometry);
-		    
-		    JSONObject aggregated = new JSONObject();
-		    for (Object temp : metrics) {
-		    	JSONObject tempJSObj = (JSONObject) temp;
-		    	for (Object temp2 : tempJSObj.keySet()) {
-		    		aggregated.put(temp2, tempJSObj.get(temp2));
-		    	}
-		    }
-		    for (Object temp : tests) {
-		    	JSONObject tempJSObj = (JSONObject) temp;
-		    	for (Object temp2 : tempJSObj.keySet()) {
-		    		aggregated.put(temp2, tempJSObj.get(temp2));
-		    	}
-		    }
-		    for (Object temp : conditions) {
-		    	JSONObject tempJSObj = (JSONObject) temp;
-		    	for (Object temp2 : tempJSObj.keySet()) {
-		    		aggregated.put(temp2, tempJSObj.get(temp2));
-		    	}
-		    }
-		    
-		    feature.addProperties(aggregated);
-		    
-		    System.out.println(feature);
-		  }
-		  catch(ParseException pe){
-		    System.out.println("position: " + pe.getPosition());
-		    System.out.println(pe);
-		  }
+		try{
+			JSONObject jsobj = (JSONObject) parser.parse(jsonText);
+			GeoJSONFeature feature = new GeoJSONFeature();
+			JSONArray tempCoordinates = new JSONArray();
+
+			String datetime = (String) jsobj.get("datetime");
+			String _sourceip = (String) jsobj.get("_sourceip");
+			String sim_operator_code = (String) jsobj.get("sim_operator_code");
+			String app_version_name = (String) jsobj.get("app_version_name");
+			String submission_type = (String) jsobj.get("submission_type");
+
+			JSONArray metrics = (JSONArray) jsobj.get("metrics");
+			JSONArray tests = (JSONArray) jsobj.get("tests");
+			JSONArray conditions = (JSONArray) jsobj.get("conditions");
+			Set coll = jsobj.entrySet();
+			for (Object temp : coll) {
+				System.out.println(temp.toString());
+			}
+
+			for (Object temp : metrics) {
+				JSONObject tempjsobj = (JSONObject) temp;
+
+				if (tempjsobj.containsKey("longitude") && tempjsobj.containsKey("latitude")) {
+					JSONArray tempArray = new JSONArray();
+					tempArray.add(tempjsobj.get("latitude"));
+					tempArray.add(tempjsobj.get("longitude"));
+
+					tempCoordinates.add(tempArray);
+
+					//System.out.println(tempjsobj.toString());
+				}
+			}
+			GeoJSONGeometry geometry = new GeoJSONGeometry("LineString", tempCoordinates);
+			feature.addGeometry(geometry);
+
+			JSONObject aggregated = new JSONObject();
+			StringBuilder sb = new StringBuilder();
+
+			for (Object temp : tests) {
+				JSONObject tempJSObj = (JSONObject) temp;
+				String testType = (String) tempJSObj.get("type");
+				if (testType.equals("JHTTPGETMT") || testType.equals("JHTTPPOSTMT")) {
+					sb.append("Target").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("target")).append(INTERNAL_DELIMITER);
+					sb.append("bytes_sec").append(INTERNAL_DESIGNATOR).append(Integer.parseInt((String) tempJSObj.get("bytes_sec"))*0.000008).append(INTERNAL_DELIMITER);
+					sb.append("success").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("success")).append(INTERNAL_DELIMITER);
+					sb.append("transfer_bytes").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("transfer_bytes")).append(INTERNAL_DELIMITER);
+					sb.append("transfer_time").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("transfer_time"));
+				}
+				else if (testType.equals("JUDPLATENCY")) {
+					sb.append("Target").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("target")).append(INTERNAL_DELIMITER);
+					sb.append("success").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("success")).append(INTERNAL_DELIMITER);
+					sb.append("rtt_avg").append(INTERNAL_DESIGNATOR).append(Integer.parseInt((String) tempJSObj.get("rtt_avg"))/1000).append(INTERNAL_DELIMITER);
+					sb.append("rtt_min").append(INTERNAL_DESIGNATOR).append(Integer.parseInt((String) tempJSObj.get("rtt_min"))/1000).append(INTERNAL_DELIMITER);
+					sb.append("rtt_max").append(INTERNAL_DESIGNATOR).append(Integer.parseInt((String) tempJSObj.get("rtt_max"))/1000).append(INTERNAL_DELIMITER);
+					sb.append("received_packets").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("received_packets")).append(INTERNAL_DELIMITER);
+					sb.append("lost_packets").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("lost_packets")).append(INTERNAL_DELIMITER);
+				}
+				else {
+					for (Object temp2 : tempJSObj.keySet()) {
+						aggregated.put(temp2, tempJSObj.get(temp2));
+					}
+				}
+				aggregated.put(testType, sb.toString());
+			}
+
+			for (Object temp : metrics) {
+				JSONObject tempJSObj = (JSONObject) temp;
+				String testType = (String) tempJSObj.get("type");
+				if (testType.equals("gsm_cell_location")) {
+					sb.append("gsm_signal_strength").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("gsm_signal_strength")).append(INTERNAL_DELIMITER);
+				}
+				else if (testType.equals("cdma_cell_location")) {
+					sb.append("dbm").append(INTERNAL_DESIGNATOR).append(tempJSObj.get("dbm")).append(INTERNAL_DELIMITER);
+				}
+				else {
+					for (Object temp2 : tempJSObj.keySet()) {
+						aggregated.put(temp2, tempJSObj.get(temp2));
+					}
+				}
+				aggregated.put(testType, sb.toString());
+			}
+
+			feature.addProperties(aggregated);
+
+			System.out.println(feature);
+		}
+		catch(ParseException pe){
+			System.out.println("position: " + pe.getPosition());
+			System.out.println(pe);
+		}
 	}
 
 }
